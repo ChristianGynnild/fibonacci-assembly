e4ebff0b714b564aa8f0c190751bad8eea0a734b
ChristianGynnild <75206913+ChristianGynnild@users.noreply.github.com>
2024-06-30 16:47:34 +0200

    Implement fibonacci sequence

--- assembly.s
+++ assembly.s
@@ -1,16 +1,49 @@
 .global _start
 .intel_syntax noprefix
 
+/*
+A (al)
+B (bl/bx)
+C (ah)
+D (dh)
+E (dl)
+*/
+
 _start:
-    mov rax, 1
-    mov rdi, 1
-    lea rsi, [hello_world]
-    mov rdx, 14
-    syscall
+    mov al, 0
+    mov bl, 0
+    mov ah, 0
+    mov dh, 0
+    mov dl, 0
 
-    mov rax, 60
-    mov rdi, 2 
+    mov dl, 1
+    mov bl, dl
 
-    syscall
+    mov dh, 1
+    mov ecx, 11 /* 11 + 2 = 13th fibonacci */
+
+    jmp fibonacci
+
+
 hello_world:
     .asciz "Hello, world!\n"
+
+
+fibonacci:
+    mov dl, bl
+    mov ah, dl
+    add bl, al 
+
+    mov dl, ah
+    mov al, dl
+
+    sub cl, dh
+    
+    JECXZ exit
+    jmp fibonacci
+
+exit:
+    mov rax, 60
+    mov rdi, 0
+    mov di, bx
+    syscall
